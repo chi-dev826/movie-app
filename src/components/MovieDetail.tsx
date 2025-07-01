1d2345fc9eee58d44fa4835fee343209c0eb0a1a
tenma <ten.ten.tenma.goal@gmail.com>
2025-07-01 22:25:33 +0900

    Fix MovieDetail.tsx type

--- src/components/MovieDetail.tsx
+++ src/components/MovieDetail.tsx
@@ -78,7 +78,7 @@ function MovieDetail() {
   const { movieId } = useParams();
   const [ similarMovieList, setSimilarMovie ] = useState<Movie[]>([]);
   const [ heroMovie, setHeroMovie ] = useState<Movie | null>(null);
-  const [ youtubeKey, setYoutubeKey] = useState();
+  const [ youtubeKey, setYoutubeKey] = useState<string | null>(null);
 
   useEffect(() => {
     const fetchMovie = async() => {
@@ -101,7 +101,9 @@ function MovieDetail() {
         runtime: data.runtime,
         score: data.vote_count,
         genres: data.genres ? data.genres.map((g: { name: string }) => g.name) : [],
-        company_logo: data.production_companies[0].logo_path,
+        company_logo: Array.isArray(data.production_companies) && data.production_companies.length > 0
+        ? data.production_companies[0].logo_path ?? ""
+        : ""
       })
     };
 
@@ -120,7 +122,6 @@ function MovieDetail() {
 
       const data = await response.json();
       setYoutubeKey(data.results[0].key);
-
     }
     fetchUrl();
   }, [movieId])
@@ -137,13 +138,13 @@ function MovieDetail() {
 
       const data = await response.json();
       const result = data.results;
+      console.log(result);
       const similarMovieList = result.map((movie: MovieJson) => ({
         id: movie.id,
         backdrop_path: movie.backdrop_path,
         original_title: movie.title,
         poster_path: movie.poster_path,
         overview: movie.overview,
-        original_language: movie.original_language,
       }));
       setSimilarMovie(similarMovieList);
     };
@@ -162,10 +163,14 @@ function MovieDetail() {
             <section className="hero-content">
               <div className="hero-metadata">
                 <div key={heroMovie.id} className="MovieDetail-overlay-contents"> 
+                  {heroMovie.company_logo && (
                   <img src={`https://image.tmdb.org/t/p/w300/${heroMovie.company_logo}`}
                        alt="logo"
                        className="company-logo" />
+                  )}
+                  {heroMovie.original_title && (
                   <h1 className="Movie-title">{heroMovie.original_title.replace(/\s+/g, "")}</h1>
+                  )}
                   <span className="MovieDetail-overlay">
                     <span>{heroMovie.year}・{heroMovie.runtime}分・{heroMovie.genres.join(",")}</span>
                   </span>
@@ -174,6 +179,7 @@ function MovieDetail() {
               </div>
               <div className="video-container">
                 <div className="video-wrapper">
+                  {youtubeKey && (
                   <iframe
                     src={`https://www.youtube.com/embed/${youtubeKey}?autoplay=1&mute=1&loop=1&rel=0&modestbranding=1&showinfo=0&controls=1`}
                     width="1200"
@@ -182,6 +188,7 @@ function MovieDetail() {
                     allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
                     allowFullScreen
                   />
+                  )}
                 </div>
               </div>
             </section>
