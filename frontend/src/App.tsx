f43af4e8e04f12806fbed7035e2ea61d1caa5818
tenma <ten.ten.tenma.goal@gmail.com>
2025-10-22 12:15:18 +0900

    fix:余白をヘッダーの高さに調整

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -5,7 +5,7 @@ function App() {
   return (
     <div className="min-h-screen text-white bg-black">
       <Header />
-      <main className="pt-16">
+      <main className="pt-14">
         {/* ヘッダーの高さ分だけコンテンツ開始位置を調整 */}
         <Outlet /> {/* ここに子ルートのコンポーネントが描画される */}
       </main>
