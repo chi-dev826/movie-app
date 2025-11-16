e862d74612d02980ca137437132a2513dc5d07dd
tenma <ten.ten.tenma.goal@gmail.com>
2025-11-16 21:32:49 +0900

    ビルド設定の変更

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,8 +1,3 @@
 import app from "./app";
 
-const port = 3000;
-const HOST = "0.0.0.0";
-
-app.listen(port, HOST, () => {
-  console.log(`Server is running on http://${HOST}:${port}`);
-});
+export default app;
