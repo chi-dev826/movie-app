53e59ec3bb45c6935340a126b9075330f0c46c71
tenma <ten.ten.tenma.goal@gmail.com>
2025-11-17 19:16:32 +0900

    ファイルの更新

--- api/routes/index.ts
+++ api/routes/index.ts
@@ -3,16 +3,6 @@ import movieRoutes from "./movie.routes";
 
 const router = Router();
 
-// デバッグ用エンドポイント
-router.get("/debug", (_req, res) => {
-  res.json({
-    message: "Checking Environment Variables",
-    has_vite_tmdb_api_key: !!process.env.VITE_TMDB_API_KEY,
-    node_env: process.env.NODE_ENV,
-    vercel_env: process.env.VERCEL_ENV,
-  });
-});
-
 router.use(movieRoutes);
 
 export default router;
