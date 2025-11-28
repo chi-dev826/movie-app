028a2eef8bd6cf3ca82df05608e3d9a3405634b7
tenma <ten.ten.tenma.goal@gmail.com>
2025-11-28 20:22:03 +0900

    fix: srcディレクトリへ移動

---
+++ api/src/constants/external.ts
@@ -0,0 +1,6 @@
+export const EXTERNAL_API_URLS = {
+  TMDB: "https://api.themoviedb.org/3",
+  YOUTUBE: "https://www.googleapis.com/youtube/v3",
+  EIGA_COM: "https://eiga.com",
+  GOOGLE_SEARCH: "https://www.googleapis.com/customsearch/v1",
+} as const;
---
+++ api/src/constants/messages.ts
@@ -0,0 +1,5 @@
+export const ERROR_MESSAGES = {
+  MOVIE_ID_REQUIRED: "Movie ID is required",
+  SEARCH_QUERY_REQUIRED: "Search query is required",
+  MOVIE_TITLE_REQUIRED: "Movie title is required",
+} as const;
