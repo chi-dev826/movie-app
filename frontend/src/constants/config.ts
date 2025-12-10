43f69068cc681c6e463e954e0534fcec66adf717
tenma <ten.ten.tenma.goal@gmail.com>
2025-12-10 12:40:45 +0900

    feat: 設定項目の追加

--- frontend/src/constants/config.ts
+++ frontend/src/constants/config.ts
@@ -24,3 +24,14 @@ export const TMDB_CONFIG = {
     },
   },
 } as const;
+
+export const EXTERNAL_URLS = {
+  YOUTUBE_WATCH: 'https://www.youtube.com/watch?v=',
+  EIGA_COM: 'https://eiga.com',
+  NETFLIX_SEARCH: 'https://www.netflix.com/search?q=',
+  APPLE_TV_SEARCH: 'https://tv.apple.com/jp/search?term=',
+  AMAZON_SEARCH: 'https://www.amazon.co.jp/s?k=',
+  AMAZON_SEARCH_PARAMS: '&i=instant-video',
+  HULU_SEARCH: 'https://www.hulu.jp/search?q=',
+  UNEXT_SEARCH: 'https://video.unext.jp/freeword?query=',
+} as const;
