57fdfbd1ba367e38c3e1982959f359f0ff9c75a7
tenma <ten.ten.tenma.goal@gmail.com>
2025-12-15 10:37:34 +0900

    ファイルの更新

---
+++ api/jest.config.ts
@@ -0,0 +1,16 @@
+/** @type {import('ts-jest/dist/types').InitialOptionsTsJest} */
+module.exports = {
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  moduleNameMapper: {
+    '^@/(.*)$': '<rootDir>/src/$1',
+  },
+  testMatch: [
+    "<rootDir>/tests/**/*.test.ts"
+  ],
+  transform: {
+    '^.+\.tsx?$': ['ts-jest', {
+      isolatedModules: true,
+    }],
+  },
+};
