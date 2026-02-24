05ce90846370a1634ac64470e12387e8dbdf12b7
chi-dev826 <ten.ten.tenma.goal@gmail.com>
2026-02-24 19:17:34 +0900

    ファイルの更新

--- api/jest.config.ts
+++ api/jest.config.ts
@@ -5,7 +5,10 @@ module.exports = {
   moduleNameMapper: {
     "^@/(.*)$": "<rootDir>/src/$1",
   },
-  testMatch: ["<rootDir>/tests/**/*.test.ts"],
+  testMatch: [
+    "<rootDir>/tests/**/*.test.ts",
+    "<rootDir>/src/domain/services/**/*.test.ts",
+  ],
   transform: {
     "^.+\\.tsx?$": [
       "ts-jest",
