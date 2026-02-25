8fa58b60c944a878fd97a7342476479b138b2cd5
chi-dev826 <ten.ten.tenma.goal@gmail.com>
2026-02-25 19:38:12 +0900

    fix: ファイルの移動

---
+++ api/src/app.ts
@@ -0,0 +1,28 @@
+import express, { NextFunction, Request, Response } from "express";
+import cors from "cors";
+import { createApiRoutes } from "./presentation/routes";
+import { HTTP_STATUS } from "../../shared/constants/httpStatus";
+import { Dependencies } from "./container";
+
+export const createApp = (dependencies: Dependencies) => {
+  const app = express();
+
+  app.use(
+    cors({
+      origin: process.env.ALLOWED_ORIGIN || "http://localhost:5173",
+    }),
+  );
+
+  app.use(express.json());
+
+  app.use("/api", createApiRoutes(dependencies));
+
+  app.use((err: Error, req: Request, res: Response, _next: NextFunction) => {
+    console.error("Error:", err.message);
+    res
+      .status(HTTP_STATUS.INTERNAL_SERVER_ERROR)
+      .json({ message: "Failed to process request", error: err.message });
+  });
+
+  return app;
+};
