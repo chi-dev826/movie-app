f2b0a3387b21304f175b7159dbe90ef490b82823
tenma <ten.ten.tenma.goal@gmail.com>
2026-04-20 22:32:28 +0900

    refactor: アニメーション変更に伴い削除

--- frontend/src/types/transitions.ts
+++
@@ -1,9 +0,0 @@
-/**
- * 画面遷移のパターン定義。
- * Route の handle プロパティで使用する。
- */
-export type TransitionType = 'horizontal' | 'vertical';
-
-export interface RouteHandle {
-  transition?: TransitionType;
-}
